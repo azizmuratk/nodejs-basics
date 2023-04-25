00f337459a0b7e7aaee026d1dd22137031f8d1ed
AzizMuratKaraca <azizkaraca0@gmail.com>
2023-04-25 09:50:01 +0200

    Request & Response Headers

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  //   process.exit();
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>Test</title></head>");
+  res.write("<body><h1>Test</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
