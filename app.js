97bc18110e6d2442bd8a3a9e2f6b9f615b8de32d
Aziz <azizkaraca0@gmail.com>
2023-04-27 13:54:25 +0200

    adding express.js

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
+const path = require("path");
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
 
-app.use("/", (req, res, next) => {
-  console.log("always running!");
-  next();
-});
-app.use("/product", (req, res, next) => {
-  res.send("<h1>Product Page!</h1>");
-});
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello form Express.JS!</h1>");
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
